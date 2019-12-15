b403da3045eee80c78007e2f1678037f3a99e8b7
Liorge Corria Sanchez <lionani07@gmail.com>
2019-12-15 00:27:11 -0300

    first commit

---
+++ src/main/java/brewer/controller/CervejasController.java
@@ -0,0 +1,35 @@
+package brewer.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import brewer.model.Cerveja;
+
+@Controller
+@RequestMapping("/cervejas")
+public class CervejasController {
+	
+	@GetMapping("/novo")
+	public ModelAndView novo(Cerveja cerveja) {
+		return new ModelAndView("cerveja/CadastroCerveja");
+	}
+	
+	@PostMapping("/novo")
+	public ModelAndView salvar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {		
+		if(result.hasErrors()) {
+			return novo(cerveja);
+		}
+		System.out.println("Cadastrar cerceja!" + cerveja.getSku());			
+		attributes.addFlashAttribute("msg", "Cerveja cadastrada com succeso");
+		return new ModelAndView("redirect:/cervejas/novo");
+	}
+	
+}
---
+++ src/main/java/brewer/model/Cerveja.java
@@ -0,0 +1,42 @@
+package brewer.model;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class Cerveja {
+	
+	@NotBlank(message = "SKU é orbigatório")
+	private String sku;
+	
+	@NotBlank(message = "Nome é obrigatório")
+	private String nome;
+	
+	@Size(min = 1, max = 50, message = "O tamanho da descriçao deve esta entre 1 e 50")
+	private String descricao;
+	
+	public String getSku() {
+		return sku;
+	}
+	
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+
+}

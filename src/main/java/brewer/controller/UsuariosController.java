65cafc61a41c0cbd1d77c0436df07bb6de51ad89
Liorge Corria Sanchez <lionani07@gmail.com>
2020-02-01 17:52:02 -0300

    Tela de CadastroUsuario e UsuariosController

---
+++ src/main/java/brewer/controller/UsuariosController.java
@@ -0,0 +1,17 @@
+package brewer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/usuarios")
+public class UsuariosController {
+
+	@GetMapping("/novo")
+	public ModelAndView novo() {
+		return new ModelAndView("usuario/CadastroUsuario");
+	}
+	
+}

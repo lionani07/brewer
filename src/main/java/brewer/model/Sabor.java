6251f56edaf47600a18c53776880ab52bc65c17c
Liorge Corria Sanchez <lionani07@gmail.com>
2020-02-06 22:59:39 -0300

    Criado modelo de novas tabelas.

---
+++ src/main/java/brewer/model/Sabor.java
@@ -0,0 +1,23 @@
+package brewer.model;
+
+public enum Sabor {
+	
+	ADOCICADA("Adocicada"),
+	AMARGA("Amarga"),
+	FORTE("Forte"),
+	FRUTADA("Frutada"),
+	SUAVE("Suave");
+	
+	private String descricao;
+	
+	Sabor(String descricao){
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return this.descricao;
+	}
+	
+	
+
+}

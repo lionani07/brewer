a4a5bbf995691ac8e78a041c57103eaad28955eb
Liorge Corria Sanchez <lionani07@gmail.com>
2020-07-10 14:13:54 -0300

    Configurando Spring security em memoria

--- src/main/java/brewer/config/SecurityConfig.java
+++ src/main/java/brewer/config/SecurityConfig.java
@@ -1,16 +1,35 @@
 package brewer.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig  extends WebSecurityConfigurerAdapter{	
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+		/* Esse aqui deu error.
+		 * return new BCryptPasswordEncoder(); 
+		 * */		
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
+	
+	@Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {  		
+    	auth
+          .inMemoryAuthentication()          
+          .withUser("admin")
+          .password(passwordEncoder.encode("admin"))
+          .roles("USER", "ADMIN");
+    }
+	
 
 }

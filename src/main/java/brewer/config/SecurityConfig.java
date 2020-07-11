3e877de0aafb3d69de9e0a71699870eb2d2bd9a2
Liorge Corria Sanchez <lionani07@gmail.com>
2020-07-10 22:49:26 -0300

    Tela de login customizada

--- src/main/java/brewer/config/SecurityConfig.java
+++ src/main/java/brewer/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package brewer.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -30,6 +32,27 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
           .password(passwordEncoder.encode("admin"))
           .roles("USER", "ADMIN");
     }
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+			.antMatchers("/layout/**")
+			.antMatchers("/images/**");
+		
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/login")
+				.permitAll()
+				.and()
+			.csrf().disable();
+	}
 	
 
 }
